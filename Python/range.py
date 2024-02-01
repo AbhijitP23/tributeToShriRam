b879e43ce199b329a6dee07553e118dc397849fa
Tushar7337 <tushar.shingde@thinkitive.com>
2024-02-01 11:45:26 +0530

    Adding Programs

---
+++ Python/range.py
@@ -0,0 +1,9 @@
+
+try:
+    x = int(input('How Many Timmes You want to print - "जय श्री राम " - '))
+
+    for i in range(x):
+        print('जय श्री राम')
+
+except Exception as e:
+    print(e)
---
+++ Python/slicing.py
@@ -0,0 +1,23 @@
+print('\nजय श्री राम')
+
+a = '|| सीता राम हनुमान ||'
+
+print('Sentence contains', len(a),'characters')
+
+b = a[3:11]
+print(b)
+
+x = a[12:19]
+print(x)
+
+# z = a[-9:-2]
+# print(z)
+
+
+locator = str("\"row\", name=\"Gwen Stacy caregiver7733+956@\"")
+
+locator1 = "row\", name=\"Damon Salvatore caregiver7733"
+
+print(locator)
+
+print(locator1)
