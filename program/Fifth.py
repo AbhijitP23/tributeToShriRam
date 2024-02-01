709efaf4abdf4d573a71e055c7d69b9cb5231ab5
LNV-90 <shubhankar.pisolkar@thinkitive.com>
2024-02-01 13:22:32 +0530

    Adding Programc

---
+++ program/Fifth.py
@@ -0,0 +1,13 @@
+
+num = int(input("Enter a number of your choice: "))
+
+if num < 35:
+    print("Fail")
+elif 35 <= num < 60:
+    print("Average")
+elif 60 <= num < 75:
+    print("Good")
+elif 75 <= num < 90:
+    print("Distinction")
+else:
+    print("|| सीता राम हनुमान ||")
---
+++ program/eighth.py
@@ -0,0 +1,5 @@
+list = ["जय श्री राम","|| सीता राम हनुमान ||"]
+print(list)
+new = "ॐ श्री हनुमते नमः"
+list.append(new)
+print(list)
