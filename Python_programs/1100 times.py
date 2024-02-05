b636e7b7ad43cb54c80d5af6403b2c676e1450d0
TusharBhadane <tushar.bhadane@thinkitive.com>
2024-02-05 10:52:30 +0530

    Adding Programs

---
+++ Python_programs/1100 times.py
@@ -0,0 +1,9 @@
+a= int(1)
+b= int(1100)
+for x in range(1100):
+    if a%108==0:
+        print("सीता राम हनुमान "+str(a))
+
+    else:
+        print("जय श्री राम "+str(a))
+    a=a+1
---
+++ Python_programs/user input.py
@@ -0,0 +1,5 @@
+num_times = int(input("Enter the number of times you want to print 'जय श्री राम': "))
+a=1
+while(a<=num_times):
+    print("जय श्री राम")
+    a=a+1
